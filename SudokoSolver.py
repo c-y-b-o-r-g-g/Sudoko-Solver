e9678a53fe9988d12383a05a1593baa034948d18
gehadkhaleeed <127341151+gehadkhaleeed@users.noreply.github.com>
2024-04-16 00:35:05 +0200

    Added New example

--- SudokoSolver.py
+++ SudokoSolver.py
@@ -7,7 +7,7 @@ def solve_sudoku(grid):
                         grid[i][j] = num
 
                         # print the grid after placing a number
-                        # print(f"Placed {num} at ({i}, {j})")
+                        print(f"Placed {num} at ({i}, {j})")
                         # for row in grid:
                         #     print(row)
                         # print("\n-----------------\n")
@@ -53,25 +53,4 @@ def is_valid_move(grid, row, col, num):
 
     return True
 
-
-    # Create a grid for testing
-grid = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
-
-    # Call the solve_sudoku function
-if solve_sudoku(grid):
-    print("Sudoku solved:")
-    for row in grid:
-        print(row)
-else:
-    print("No solution exists for the given Sudoku puzzle.")
 
---
+++ example.py
@@ -0,0 +1,22 @@
+import SudokoSolver
+# Create a grid for testing
+grid = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+    # Call the solve_sudoku function
+if solve_sudoku(grid):
+    print("Sudoku solved:")
+    for row in grid:
+        print(row)
+else:
+    print("No solution exists for the given Sudoku puzzle.")
+
